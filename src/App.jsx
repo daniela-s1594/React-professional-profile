0955f72dc93f72b8c19754620c3d6feedd2c58d1
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-05-07 18:22:39 -0500

    Creacion y modificacion de proyecto en react.DMV

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Perfil Profesional</h1>
+      <h3>Sobre mí</h3>
+      <p>Un individuo honesto, organizado, creativo, imaginativo y puntual con la capacidad de pensar y crear habilidades con conocimientos de TI. Buscando la oportunidad de aprovechar las habilidades y la experiencia adquirida al trabajar en pequeños proyectos y establecer una carrera como desarrollador.
+      </p>
+      <h3>Datos personales</h3>
+      <p>Nombre: Daniela Manjarrés Virviescas</p>
+      <p>Profesión: Desarrollador de Software</p>
+      <p>Fecha de nacimiento: Septiembre 15 del 2004</p>
+      <p>Edad:15</p>
+      <p>Nacionalidad:Colombiana</p>
+      <h3>Contacto</h3>
+      <p>Teléfono:(57+)323 3198140</p>
+      <p>Correo:dmanjarres18@misena.edu.co</p>
+      <p>Dirección:Calarca,Quindio</p>
+      <h3>Lenguajes</h3>
+      <p>Inglés B1</p>
+      <p>Español</p>
+      <h3>Habilidades</h3>
+      <ul>
+        <li>Creatividad</li>
+        <li>Resiliencia</li>
+        <li>Buena organización</li> 
+        <li>Solución de problemas</li>  
+        <li>Buena habladora</li>
+        <li>HTML 60%</li> 
+        <li>CSS 40%</li> 
+        <li>JAVASCRIPT 47%</li>
+        <li>MySQL Workbench 50%</li> 
+        <li>C# 20%</li> 
+        <li>SQL Server 50%</li>
+      </ul>
+      <h3>Pasatiempos</h3>
+      <ul>
+        <li>Leer</li>
+        <li>Crochet</li>
+        <li>Ver peliculas</li>
+        <li>Hacer manualidades variadas</li>
+      </ul>
+      </div>
+    
+  );
+}
+
+export default App;
+
